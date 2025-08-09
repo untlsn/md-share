6c70b7ed0ef4ffca45a7c8d17023df9a31b0401a
Filip Skoczen <philipskoczen@gmail.com>
2025-08-09 16:33:45 +0200

    Change static to prerender

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,7 +24,9 @@ export default defineNuxtConfig({
   },
   compatibilityDate: '2025-07-15',
   nitro: {
-    static: true,
+    prerender: {
+      routes: ['/'],
+    },
   },
   vite: {
     plugins: [
